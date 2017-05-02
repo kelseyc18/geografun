384a9af2b1ae5bb3ee4bc68fb5d47dcb85bbe276
Philip Caplan <philip@dhcp-18-189-62-75.dyn.mit.edu>
2017-05-02 10:35:29 -0400

    evidence window popup

--- src/geografun.js
+++ src/geografun.js
@@ -80,10 +80,93 @@ function makeClue(clue)
 
   box.style.left = clue.position[0]+"px";
   box.style.top = clue.position[1]+"px";
+  box.style.display = "block";
+
+  box.onclick = function() { makeEvidenceWindow(clue.data); };
+  //box.data = clue.data;
 
   // set onclick function for clue
+  //setEvidenceWindow(box,clue.data);
 
   viewer.appendChild(box);
+
+}
+
+function fillEvidence(doc,data)
+{
+  text = doc.createElement("p");
+  text.innerHTML = data.text;
+  doc.body.appendChild(text);
+
+  if (data.image!=null)
+  {
+    // display the image
+  }
+
+  if (data.list!=null)
+  {
+    // display the list as radio boxes
+    // make a table
+    var t = doc.createElement('table');
+    list = data.list;
+
+    for (i in list)
+    {
+      item = list[i];
+      var row = t.insertRow(i);
+      var cbox = row.insertCell(0);
+      var text = row.insertCell(1);
+
+      cbox.innerHTML = "<input type='checkbox'></input>";
+      text.innerHTML = item;
+    }
+    doc.body.appendChild(t);
+
+  }
+}
+
+function makeWindowAtCenter(url, title, w, h) {
+    // Fixes dual-screen position                         Most browsers      Firefox
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+    var top = ((height / 2) - (h / 2)) + dualScreenTop;
+    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+    // Puts focus on the newWindow
+    if (window.focus) {
+        newWindow.focus();
+    }
+    return newWindow;
+}
+
+function makeSubmitButton(doc)
+{
+  var submit = doc.createElement("BUTTON");
+  submit.innerHTML = "submit";
+
+  submit.style.width = "50px";
+  submit.style.height = "20px";
+  submit.style.display = "block"; // for centering
+
+
+  doc.body.appendChild(submit);
+}
+
+function makeEvidenceWindow(data)
+{
+  console.log(data);
+  var evidence = makeWindowAtCenter("","evidence",data.size[0],data.size[1]); //window.open("","evidence","width=200,height=200");
+
+  evidence.document.title = "Evidence";
+
+  fillEvidence(evidence.document,data);
+
+  makeSubmitButton(evidence.document);
 
 }
 
@@ -92,9 +175,6 @@ function gfLayClues(clues)
   for (i in clues)
   {
     var clue = clues[i];
-    console.log(clue);
-    console.log("laying clue at "+clue.position);
-
     makeClue(clue);
   }
 }
